178fcd2d7e1c9f61c4c1781493cc74e7ca65fc89
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-06-05 15:32:13 +0200

    Create a route to list purchasables for a show

--- backend/recom/lib/recom_web/router.ex
+++ backend/recom/lib/recom_web/router.ex
@@ -17,6 +17,8 @@ defmodule RecomWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+
+    get "/shows/:id/purchasables", PurchasablesController, :list
   end
 
   # Other scopes may use custom stacks.
