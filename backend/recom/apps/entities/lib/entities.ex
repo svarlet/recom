cb358416438c6950485cad889bda2934e534c2b2
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-06-14 17:05:23 +0100

    Improved Product.new to accept a keyword to intialize the returned product

--- backend/recom/apps/entities/lib/entities.ex
+++ backend/recom/apps/entities/lib/entities.ex
@@ -2,8 +2,8 @@ defmodule Recom.Entities do
   defmodule Product do
     defstruct start: nil, end: nil, name: ""
 
-    def new() do
-      __struct__()
+    def new(fields \\ []) do
+      __struct__(fields)
     end
   end
 end
--- backend/recom/apps/entities/test/product_test.exs
+++ backend/recom/apps/entities/test/product_test.exs
@@ -17,4 +17,21 @@ defmodule Recom.Entities.ProductTest do
     end
   end
 
+  describe "new/1" do
+    test "given a keyword list with a start key then a product with the associated start value is returned" do
+      instant = ~N[2017-09-14 13:00:02]
+      assert %Product{start: ^instant} = Product.new(start: instant)
+    end
+
+    test "given a keyword list with an end key then a product with the associated end value is returned" do
+      instant = ~N[2019-01-23 23:00:12]
+      assert %Product{end: ^instant} = Product.new(end: instant)
+    end
+
+    test "given a keyword list with a name key then a product with the associated name value is returned" do
+      name = "irrelevant name"
+      assert %Product{name: ^name} = Product.new(name: name)
+    end
+  end
+
 end
