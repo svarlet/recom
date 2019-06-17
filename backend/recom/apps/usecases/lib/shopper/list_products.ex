a2d09e1e1449d316d31dc4713d0812132cce5519
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-06-17 10:31:29 +0100

    List products returns products scheduled in the future.

--- backend/recom/apps/usecases/lib/shopper/list_products.ex
+++ backend/recom/apps/usecases/lib/shopper/list_products.ex
@@ -1,5 +1,7 @@
 defmodule Usecases.Shopper.ListProducts do
   def list_products(products, instant) do
-    []
+    for p <- products, NaiveDateTime.compare(instant, p.start) == :lt do
+      p
+    end
   end
 end
--- backend/recom/apps/usecases/test/shopper/list_products_test.exs
+++ backend/recom/apps/usecases/test/shopper/list_products_test.exs
@@ -7,11 +7,34 @@ defmodule Usecases.Shopper.ListProductsTest do
 
   test "it returns [] when no products are available at the specified instant" do
     products = [
-      Product.new(name: "M2020 EU 2018 Regular Pass", start: ~N[2018-05-31 09:00:00], end: ~N[2019-06-02 17:00:00]),
-      Product.new(name: "M2020 EU 2019 Regular Pass", start: ~N[2019-06-02 09:00:00], end: ~N[2019-06-05 17:00:00]),
-      Product.new(name: "M2020 EU 2019 VIP Pass", start: ~N[2019-06-02 09:00:00], end: ~N[2019-06-05 17:00:00])
+      Product.new(name: "M2020 EU 2018 Regular Pass",
+        start: ~N[2018-05-31 09:00:00],
+        end: ~N[2018-06-02 17:00:00]),
+      Product.new(name: "M2020 EU 2019 Regular Pass",
+        start: ~N[2019-06-02 09:00:00],
+        end: ~N[2019-06-05 17:00:00]),
+      Product.new(name: "M2020 EU 2019 VIP Pass",
+        start: ~N[2019-06-02 09:00:00],
+        end: ~N[2019-06-05 17:00:00])
     ]
     instant_after_last_scheduled_event = ~N[2019-06-05 17:01:00]
     assert [] == list_products(products, instant_after_last_scheduled_event)
+  end
+
+  test "it returns the products scheduled in the future" do
+    p1 = Product.new(name: "M2020 EU 2018 Regular Pass",
+      start: ~N[2018-05-31 09:00:00],
+      end: ~N[2018-06-02 17:00:00])
+    p2 = Product.new(name: "M2020 EU 2019 Regular Pass",
+      start: ~N[2019-06-02 09:00:00],
+      end: ~N[2019-06-05 17:00:00])
+    p3 = Product.new(name: "M2020 EU 2019 VIP Pass",
+      start: ~N[2019-06-02 09:00:00],
+      end: ~N[2019-06-05 17:00:00])
+    products = [p1, p2, p3]
+    instant_before_some_scheduled_products = ~N[2019-04-14 14:00:00]
+    upcoming_products = list_products(products, instant_before_some_scheduled_products)
+    assert p2 in upcoming_products
+    assert p3 in upcoming_products
   end
 end
