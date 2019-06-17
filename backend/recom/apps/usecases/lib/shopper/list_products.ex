5dd52b455de2cc37815a4be6138606c803220c31
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-06-17 12:17:38 +0100

    Fix Feature Envy in ListProducts by using the new Product.purchasable? function

--- backend/recom/apps/usecases/lib/shopper/list_products.ex
+++ backend/recom/apps/usecases/lib/shopper/list_products.ex
@@ -1,6 +1,8 @@
 defmodule Usecases.Shopper.ListProducts do
+  alias Entities.Product
+
   def list_products(products, instant) do
-    for p <- products, NaiveDateTime.compare(instant, p.end) == :lt do
+    for p <- products, Product.purchasable?(p, instant) do
       p
     end
   end
