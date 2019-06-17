b6b0015a3b010d72a46261f80993b892068fb52d
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-06-17 23:38:57 +0100

    Created a SSL certificate for running dev on https

--- backend/recom/apps/api/config/dev.exs
+++ backend/recom/apps/api/config/dev.exs
@@ -7,7 +7,12 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :api, ApiWeb.Endpoint,
-  http: [port: 4000],
+  https: [
+    port: 4000,
+    cipher_suite: :strong,
+    certfile: "priv/cert/selfsigned.pem",
+    keyfile: "priv/cert/selfsigned_key.pem"
+  ],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
