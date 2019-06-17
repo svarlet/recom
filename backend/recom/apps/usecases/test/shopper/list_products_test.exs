ad81cea8557375cdf416e11446721a93931bbc57
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-06-17 10:55:51 +0100

    Added a product to the fixtures to make the tests a bit more specific

--- backend/recom/apps/usecases/test/shopper/list_products_test.exs
+++ backend/recom/apps/usecases/test/shopper/list_products_test.exs
@@ -15,23 +15,26 @@ defmodule Usecases.Shopper.ListProductsTest do
     p3 = Product.new(name: "M2020 EU 2019 VIP Pass",
       start: ~N[2019-06-02 09:00:00],
       end: ~N[2019-06-05 17:00:00])
-    %{products: [p1, p2, p3], p1: p1, p2: p2, p3: p3}
+    p4 = Product.new(name: "M2020 EU 2020 Regular Pass",
+      start: ~N[2020-07-02 09:00:00],
+      end: ~N[2020-07-05 17:00:00])
+    %{products: [p1, p2, p3, p4], p1: p1, p2: p2, p3: p3, p4: p4}
   end
 
   test "it returns [] when no products are available at the specified instant", context do
-    instant_after_last_scheduled_event = ~N[2019-06-05 17:01:00]
+    instant_after_last_scheduled_event = ~N[2052-06-05 17:01:00]
     assert [] == list_products(context.products, instant_after_last_scheduled_event)
   end
 
   test "it returns the products scheduled in the future", context do
-    instant_before_some_scheduled_products = ~N[2019-04-14 14:00:00]
+    instant_before_some_scheduled_products = ~N[2020-04-14 14:00:00]
     upcoming_products = list_products(context.products, instant_before_some_scheduled_products)
-    assert [context.p2, context.p3] == upcoming_products
+    assert [context.p4] == upcoming_products
   end
 
   test "it returns the products spanning over the instant", context do
     instant_within_scheduled_products = ~N[2019-06-03 16:00:00]
     upcoming_products = list_products(context.products, instant_within_scheduled_products)
-    assert [context.p2, context.p3] == upcoming_products
+    assert [context.p2, context.p3, context.p4] == upcoming_products
   end
 end
