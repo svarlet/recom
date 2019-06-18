b9be0f47b646895ca984809ba866606c70ccaf89
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-06-18 23:21:18 +0100

    Configured the API app to run a HTTPS server with Plug and Cowboy and self signed certificates.

--- backend/recom/apps/api/lib/api/application.ex
+++ backend/recom/apps/api/lib/api/application.ex
@@ -10,6 +10,15 @@ defmodule Api.Application do
     children = [
       # Starts a worker by calling: Api.Worker.start_link(arg)
       # {Api.Worker, arg}
+      Plug.Cowboy.child_spec(
+        scheme: :https,
+        plug: Api.Router,
+        options: [
+          port: 4001,
+          otp_app: :api,
+          certfile: "priv/cert/selfsigned.pem",
+          keyfile: "priv/cert/selfsigned_key.pem"
+        ])
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
