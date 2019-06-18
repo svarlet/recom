c7b99cd24d6b3192d1ffa43b9269ea073e78a72c
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-06-18 17:20:59 +0100

    Improve readability of test fixtures

--- backend/recom/apps/storage/test/products_test.exs
+++ backend/recom/apps/storage/test/products_test.exs
@@ -13,20 +13,15 @@ defmodule Storage.PurchasablesGatewayTest do
   test "given some products, when some expire after the instant, it returns those" do
     instant = Timex.now()
 
-    past_product = %Product{
-      name: "Pass 2015",
-      start: Timex.shift(instant, days: -5),
-      end: Timex.shift(instant, days: -2)}
-
-    {:ok, _product} = Storage.Repo.insert(past_product)
-
-    future_product = %Product{
-      name: "Future Pass starting in 3 years from now and lasting 2 days",
-      start: Timex.shift(instant, years: 3),
-      end: Timex.shift(instant, years: 3, days: 2)
-    }
-
-    {:ok, _product} = Storage.Repo.insert(future_product)
+    [%Product{
+        name: "Pass 2015",
+        start: Timex.shift(instant, days: -5),
+        end: Timex.shift(instant, days: -2)},
+     %Product{
+       name: "Future Pass starting in 3 years from now and lasting 2 days",
+       start: Timex.shift(instant, years: 3),
+       end: Timex.shift(instant, years: 3, days: 2)}]
+    |> Enum.each(&Storage.Repo.insert/1)
 
     future_product_as_entity = Entities.Product.new(
       name: "Future Pass starting in 3 years from now and lasting 2 days",
