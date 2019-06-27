78f9209bd83163dd92dbf28d5b9c840605ac9399
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-06-27 16:18:31 +0100

    Extract the creation of the instant to a setup function

--- backend/recom/apps/usecases/test/shopper/list_purchasables_test.exs
+++ backend/recom/apps/usecases/test/shopper/list_purchasables_test.exs
@@ -27,35 +27,38 @@ defmodule Usecases.Shopper.ListPurchasablesTest do
   # Contract tests
   #
 
-  test "on success, returns the found purchasables" do
-    instant = Timex.now()
-    found_purchasables = [
-      Product.new(
-        name: "irrelevant name 1",
-        time_span: Interval.new(from: Timex.shift(instant, days: 2), until: [days: 3])),
-      Product.new(
+  describe "given multiple purchasables are available, when retrieved successfully" do
+    setup do
+      %{instant: Timex.now()}
+    end
+
+    test "on success, returns the found purchasables", context do
+      found_purchasables = [
+        Product.new(
+          name: "irrelevant name 1",
+          time_span: Interval.new(from: Timex.shift(context.instant, days: 2), until: [days: 3])),
+        Product.new(
+          name: "irrelevant name 2",
+        time_span: Interval.new(from: Timex.shift(context.instant, months: 1), until: [months: 1, days: 2]))
+      ]
+      Mox.stub(PurchasablesGateway.MockAdapter, :all, fn _instant -> {:ok, found_purchasables} end)
+      assert {:ok, found_purchasables} == list_purchasables(context.instant, PurchasablesGateway.MockAdapter)
+    end
+
+    test "given multiple purchasables are availalbe when retrieved then they are sorted by start date and returned", context do
+      later_purchasable = Product.new(
         name: "irrelevant name 2",
-      time_span: Interval.new(from: Timex.shift(instant, months: 1), until: [months: 1, days: 2]))
-    ]
-    Mox.stub(PurchasablesGateway.MockAdapter, :all, fn _instant -> {:ok, found_purchasables} end)
-    assert {:ok, found_purchasables} == list_purchasables(instant, PurchasablesGateway.MockAdapter)
-  end
+        time_span: Interval.new(from: Timex.shift(context.instant, months: 1), until: [months: 1, days: 2]))
 
-  test "given multiple purchasables are availalbe when retrieved then they are sorted by start date and returned" do
-    instant = Timex.now()
+      soon_purchasable = Product.new(
+        name: "irrelevant name 1",
+        time_span: Interval.new(from: Timex.shift(context.instant, days: 2), until: [days: 3]))
 
-    later_purchasable = Product.new(
-      name: "irrelevant name 2",
-      time_span: Interval.new(from: Timex.shift(instant, months: 1), until: [months: 1, days: 2]))
-
-    soon_purchasable = Product.new(
-      name: "irrelevant name 1",
-      time_span: Interval.new(from: Timex.shift(instant, days: 2), until: [days: 3]))
-
-    # purposedly sorted by their start date in descending order!
-    found_purchasables = [later_purchasable, soon_purchasable]
-    Mox.stub(PurchasablesGateway.MockAdapter, :all, fn _instant -> {:ok, found_purchasables} end)
-    assert {:ok, [soon_purchasable, later_purchasable]} == list_purchasables(instant, PurchasablesGateway.MockAdapter)
+      # purposedly sorted by their start date in descending order!
+      found_purchasables = [later_purchasable, soon_purchasable]
+      Mox.stub(PurchasablesGateway.MockAdapter, :all, fn _instant -> {:ok, found_purchasables} end)
+      assert {:ok, [soon_purchasable, later_purchasable]} == list_purchasables(context.instant, PurchasablesGateway.MockAdapter)
+    end
   end
 
   test "on failure, relays the error" do
