dc42ec4e898ca9858c3f3c00f8ba4843664a869b
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-06-27 16:53:12 +0100

    Use Product.before?/2 to simplify the usecase

--- backend/recom/apps/usecases/lib/shopper/list_purchasables.ex
+++ backend/recom/apps/usecases/lib/shopper/list_purchasables.ex
@@ -6,13 +6,12 @@ defmodule Usecases.Shopper do
   end
 
   defmodule ListPurchasables do
+    alias Entities.Product
+
     def list_purchasables(instant, purchasables_gateway) do
       case purchasables_gateway.all(instant) do
         {:ok, purchasables} ->
-          purchasables_sorted_by_start_date =
-            purchasables
-            |> Enum.sort_by(fn p -> p.time_span.from end, &Timex.before?/2)
-          {:ok, purchasables_sorted_by_start_date}
+          {:ok, Enum.sort(purchasables, &Product.before?/2)}
 
         {:error, reason} ->
           {:error, reason}
