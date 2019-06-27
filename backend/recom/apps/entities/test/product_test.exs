556a09fb3a781a694f9e769547d5854ab067e1d7
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-06-27 16:46:03 +0100

    Product.before?/2 returns false when the first product starts after the second

--- backend/recom/apps/entities/test/product_test.exs
+++ backend/recom/apps/entities/test/product_test.exs
@@ -50,5 +50,20 @@ defmodule Entities.ProductTest do
           until: [days: 1]))
       assert Product.before?(product_sooner, product_later)
     end
+
+    test "returns false when the first product starts after the second product" do
+      now = Timex.now()
+      product_sooner = Product.new(
+        name: "irrelevant",
+        time_span: Timex.Interval.new(
+          from: now,
+          until: [days: 1]))
+      product_later = Product.new(
+        name: "irrelevant",
+        time_span: Timex.Interval.new(
+          from: Timex.shift(now, hours: 2),
+          until: [days: 1]))
+      refute Product.before?(product_later, product_sooner)
+    end
   end
 end
