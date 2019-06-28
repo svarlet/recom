8d5947a750d9736166bc521b7ce2995501c6546a
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-06-28 14:42:12 +0100

    Found an extra way to make the DbAdapter raise an error in tests.

--- backend/recom/apps/storage/test/purchasables/db_adapter_error_test.exs
+++ backend/recom/apps/storage/test/purchasables/db_adapter_error_test.exs
@@ -5,5 +5,11 @@ defmodule Storage.PurchasablesGateway.DbAdapter_ErrorTest do
     test "when Ecto raises an error then it returns :error" do
       assert :error == Storage.PurchasablesGateway.DbAdapter.all("NOT A DATETIME, THAT WILL RAISE")
     end
+
+    test "close tx" do
+      # Checkin the transation. That raises a DBConnection.OwnershipError.
+      Ecto.Adapters.SQL.Sandbox.checkin(Storage.Repo)
+      assert :error == Storage.PurchasablesGateway.DbAdapter.all(DateTime.utc_now())
+    end
   end
 end
