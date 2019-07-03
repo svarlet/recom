ca5e3a235ae06f6893e9d4007fa80ca1dcc4cd12
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-07-03 13:11:18 +0100

    Shrunk the test case by using factoring out the test setup from the tests

--- backend/recom/test/usecases/shopkeeper/create_product_invalid_request_test.exs
+++ backend/recom/test/usecases/shopkeeper/create_product_invalid_request_test.exs
@@ -4,74 +4,57 @@ defmodule Recom.Usecases.Shopkeeper.CreateProduct_InvalidRequest_Test do
 
   alias Recom.Usecases.Shopkeeper.CreateProduct
 
-  setup do
-    %{
-      notification_service: nil,
-      products_gateway: nil,
-      request: %CreateProduct.Request{
+  setup context do
+    valid_request =
+      %CreateProduct.Request{
         price: 100,
         interval: Interval.new(from: Timex.now(), until: [days: 1]),
         quantity: 45,
         name: "irrelevant"
       }
-    }
+
+    overrides = Map.take(context, ~w{price quantity name interval}a)
+
+    {:error, {:validation, validation_errors}} =
+      CreateProduct.create(
+        Map.merge(valid_request, overrides),
+        notification_service: nil,
+        products_gateway: nil
+      )
+
+    %{validation_errors: validation_errors}
   end
 
+  @tag price: -100
   test "given a negative price, it returns {:error, {:validation, :negative_price}}", context do
-    {:error, {:validation, validation_errors}} =
-      CreateProduct.create(
-        %CreateProduct.Request{context.request | price: -100},
-        notification_service: context.notification_service,
-        products_gateway: context.products_gateway
-      )
-
-    assert_negative_price_reported(validation_errors)
+    assert_negative_price_reported(context.validation_errors)
   end
 
-  test "given a negative quantity, it returns {:error, {:validation, :negative_quantity}}",
-       context do
-    {:error, {:validation, validation_errors}} =
-      CreateProduct.create(
-        %CreateProduct.Request{context.request | quantity: -1},
-        notification_service: context.notification_service,
-        products_gateway: context.products_gateway
-      )
-
-    assert_negative_quantity_reported(validation_errors)
+  @tag quantity: -1
+  test "given a negative quantity, it returns {:error, {:validation, :negative_quantity}}", context do
+    assert_negative_quantity_reported(context.validation_errors)
   end
 
+  @tag quantity: -2
+  @tag price: -24
   test "given a negative price and a negative quantity, it returns both errors", context do
-    {:error, {:validation, validation_errors}} =
-      CreateProduct.create(
-        %CreateProduct.Request{context.request | quantity: -2, price: -24},
-        notification_service: context.notification_service,
-        products_gateway: context.products_gateway
-      )
-
-    assert_negative_price_reported(validation_errors)
-    assert_negative_quantity_reported(validation_errors)
+    assert_negative_price_reported(context.validation_errors)
+    assert_negative_quantity_reported(context.validation_errors)
   end
 
+  @tag name: ""
   test "given an empty name, it returns an error", context do
-    {:error, {:validation, validation_errors}} =
-      CreateProduct.create(
-        %CreateProduct.Request{context.request | name: ""},
-        notification_service: context.notification_service,
-        products_gateway: context.products_gateway
-      )
-
-    assert_empty_name_reported(validation_errors)
+    assert_empty_name_reported(context.validation_errors)
   end
 
+  @tag name: "    \n  \t \r"
   test "if after trimming the name is empty, it returns an error", context do
-    {:error, {:validation, validation_errors}} =
-      CreateProduct.create(
-        %CreateProduct.Request{context.request | name: "    \n  \t \r"},
-        notification_service: context.notification_service,
-        products_gateway: context.products_gateway)
-
-    assert_empty_name_reported(validation_errors)
+    assert_empty_name_reported(context.validation_errors)
   end
+
+  #
+  # Custom Assertions
+  #
 
   defp assert_negative_quantity_reported(errors) do
     assert {:quantity, [:negative]} in errors
