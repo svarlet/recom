99e50ed942524ca3e75a63849f9094f4ddd32ee6
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-07-05 16:13:52 +0100

    Reworking the test list of the create product usecase

--- backend/recom/test/usecases/shopkeeper/create_product/create_product_test.exs
+++ backend/recom/test/usecases/shopkeeper/create_product/create_product_test.exs
@@ -10,30 +10,52 @@ defmodule Recom.Usecases.Shopkeeper.CreateProductTest do
 
   defmock(CreateProduct.ValidatorMock, for: CreateProduct.ValidatorBehaviour)
 
-  #
-  # Collaboration tests
-  #
-  test "it validates the request" do
-    request = %CreateProduct.Request{
-      name: "irrelevant",
-      price: 20,
-      quantity: 100,
-      interval: Interval.new(from: Timex.now(), until: [hours: 2])
-    }
+  # test "it validates the request" do
+  #   request = %CreateProduct.Request{
+  #     name: "irrelevant",
+  #     price: 20,
+  #     quantity: 100,
+  #     interval: Interval.new(from: Timex.now(), until: [hours: 2])
+  #   }
 
-    expect(CreateProduct.ValidatorMock, :validate, fn ^request -> {:validation, []} end)
+  #   expect(CreateProduct.ValidatorMock, :validate, fn ^request -> {:validation, []} end)
 
-    CreateProduct.create(request, with_validator: CreateProduct.ValidatorMock)
+  #   CreateProduct.create(request, with_validator: CreateProduct.ValidatorMock)
+  # end
+
+  describe "empty catalog" do
+    @tag :skip
+    test "it creates the product"
+
+    @tag :skip
+    test "it dispatches a notification"
   end
 
-  #
-  # Contract tests
-  #
+  describe "original product" do
+    @tag :skip
+    test "it creates the product"
 
-  test "when validation fails, it returns a detailed error" do
-    irrelevant_request = nil
-    stub(CreateProduct.ValidatorMock, :validate, fn _ -> {:validation, name: [:empty]} end)
-    result = CreateProduct.create(irrelevant_request, with_validator: CreateProduct.ValidatorMock)
-    assert {:error, {:validation, name: [:empty]}} == result
+    @tag :skip
+    test "it dispatches a notification"
+  end
+
+  describe "dupplicate product" do
+    @tag :skip
+    test "it returns an error"
+
+    @tag :skip
+    test "it dispatches a warning notification"
+  end
+
+  describe "semantically invalid request" do
+    test "it returns an error" do
+      stub(CreateProduct.ValidatorMock, :validate, fn :invalid_request ->
+        {:validation, name: [:empty]}
+      end)
+
+      result = CreateProduct.create(:invalid_request, with_validator: CreateProduct.ValidatorMock)
+
+      assert {:error, {:validation, name: [:empty]}} == result
+    end
   end
 end
