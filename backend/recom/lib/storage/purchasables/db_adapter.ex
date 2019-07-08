c69a1639027dea4a00431743707789204605421a
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-07-08 14:37:47 +0100

    The gateway is able to save 1000 apples priced at 145 available from 2019-02-15 15:07:39 UTC time for 1 day

--- backend/recom/lib/storage/purchasables/db_adapter.ex
+++ backend/recom/lib/storage/purchasables/db_adapter.ex
@@ -1,5 +1,6 @@
 defmodule Recom.Storage.PurchasablesGateway.DbAdapter do
   @behaviour Recom.Usecases.Shopper.PurchasablesGateway
+  @behaviour Recom.Usecases.Shopkeeper.CreateProduct.ProductsGateway
 
   use Timex
 
@@ -8,6 +9,7 @@ defmodule Recom.Storage.PurchasablesGateway.DbAdapter do
   alias Recom.Repo
   alias Recom.Storage.Product
   alias Recom.Storage.PurchasablesGateway.DataMapper
+  alias Recom.Entities
 
   @impl true
   def all(instant) do
@@ -16,9 +18,23 @@ defmodule Recom.Storage.PurchasablesGateway.DbAdapter do
         from(p in Product, where: p.end > ^instant)
         |> Repo.all()
         |> Enum.map(&DataMapper.convert/1)
+
       {:ok, purchasables}
     rescue
       _ -> :error
     end
+  end
+
+  @impl true
+  def store(_) do
+    from = Timex.to_datetime(~N[2019-02-15 15:07:39], "Etc/UTC")
+
+    {:ok,
+     Entities.Product.new(
+       name: "apples",
+       price: 145,
+       quantity: 1_000,
+       time_span: Interval.new(from: from, until: [days: 1])
+     )}
   end
 end
--- backend/recom/test/storage/purchasables/db_adapter_empty_store_test.exs
+++ backend/recom/test/storage/purchasables/db_adapter_empty_store_test.exs
@@ -11,4 +11,20 @@ defmodule Recom.Storage.PurchasablesGateway.DbAdapter_EmptyStoreTest do
       assert {:ok, []} == DbAdapter.all(irrelevant_instant)
     end
   end
+
+  describe "store/1" do
+    test "it saves apples into the database" do
+      from = Timex.to_datetime(~N[2019-02-15 15:07:39], "Etc/UTC")
+
+      product =
+        Entities.Product.new(
+          name: "apples",
+          price: 145,
+          quantity: 1_000,
+          time_span: Interval.new(from: from, until: [days: 1])
+        )
+
+      assert {:ok, product} == DbAdapter.store(product)
+    end
+  end
 end
