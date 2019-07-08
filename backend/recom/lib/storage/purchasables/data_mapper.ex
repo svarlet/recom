51ba3c6844b3c875938eacd4156de592e8d3cd44
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-07-08 16:48:10 +0100

    Update the data mapper to include price and quantity in the conversion

--- backend/recom/lib/storage/purchasables/data_mapper.ex
+++ backend/recom/lib/storage/purchasables/data_mapper.ex
@@ -3,9 +3,18 @@ defmodule Recom.Storage.PurchasablesGateway.DataMapper do
 
   alias Recom.{Entities, Storage}
 
-  def convert(%Storage.Product{name: name, start: start, end: the_end}) do
+  def convert(%Storage.Product{
+        name: name,
+        start: start,
+        end: the_end,
+        price: price,
+        quantity: quantity
+      }) do
     Entities.Product.new(
       name: name,
-      time_span: Interval.new(from: start, until: the_end))
+      price: price,
+      quantity: quantity,
+      time_span: Interval.new(from: start, until: the_end)
+    )
   end
 end
--- backend/recom/test/storage/purchasables/data_mapper_test.exs
+++ backend/recom/test/storage/purchasables/data_mapper_test.exs
@@ -8,8 +8,22 @@ defmodule Recom.Storage.PurchasablesGateway.DataMapperTest do
   test "converts a Product schema into a Product entity" do
     start = Timex.now()
     the_end = Timex.shift(start, hours: 1)
-    schema = %Storage.Product{name: "the name", start: start, end: the_end}
-    entity = %Entities.Product{name: "the name", time_span: Interval.new(from: start, until: the_end)}
+
+    schema = %Storage.Product{
+      name: "the name",
+      start: start,
+      end: the_end,
+      price: 123,
+      quantity: 345
+    }
+
+    entity = %Entities.Product{
+      name: "the name",
+      time_span: Interval.new(from: start, until: the_end),
+      price: 123,
+      quantity: 345
+    }
+
     assert entity == DataMapper.convert(schema)
   end
 end
