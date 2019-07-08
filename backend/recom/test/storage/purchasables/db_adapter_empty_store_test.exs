0c74874b7f4beb2271cdc759cdd78e371d637589
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-07-08 14:27:24 +0100

    Wrap the preexising test in a describe block

--- backend/recom/test/storage/purchasables/db_adapter_empty_store_test.exs
+++ backend/recom/test/storage/purchasables/db_adapter_empty_store_test.exs
@@ -2,10 +2,13 @@ defmodule Recom.Storage.PurchasablesGateway.DbAdapter_EmptyStoreTest do
   use Recom.Storage.DataCase
   use Timex
 
+  alias Recom.Entities
   alias Recom.Storage.PurchasablesGateway.DbAdapter
 
-  test "given no products, when requesting all purchasables, it returns {:ok, []}" do
-    irrelevant_instant = Timex.now()
-    assert {:ok, []} == DbAdapter.all(irrelevant_instant)
+  describe "all/1" do
+    test "given no products, when requesting all purchasables, it returns {:ok, []}" do
+      irrelevant_instant = Timex.now()
+      assert {:ok, []} == DbAdapter.all(irrelevant_instant)
+    end
   end
 end
