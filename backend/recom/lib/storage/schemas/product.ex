bc7834dc205aecc4dacbe2b8095dd8c50b778cbf
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-07-08 13:17:38 +0100

    Declare the price and quantity fields in the Product schema

--- backend/recom/lib/storage/schemas/product.ex
+++ backend/recom/lib/storage/schemas/product.ex
@@ -2,10 +2,12 @@ defmodule Recom.Storage.Product do
   use Ecto.Schema
 
   schema "products" do
-    field :name, :string
-    field :start, :utc_datetime_usec
-    field :end, :utc_datetime_usec
+    field(:name, :string)
+    field(:start, :utc_datetime_usec)
+    field(:end, :utc_datetime_usec)
+    field(:price, :integer)
+    field(:quantity, :integer)
 
-    timestamps type: :utc_datetime_usec
+    timestamps(type: :utc_datetime_usec)
   end
 end
