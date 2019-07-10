30a35c9ff15aa0af67c3538ff6092615b5ad1a89
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-07-10 16:06:26 +0100

    Delete validation of a product shape from the usecase. This can, and should, be handled by the controller. The usecase still perform some semantic checking such as "is it a duplicate?" though.

--- backend/recom/lib/usecases/shopkeeper/create_product.ex
+++ backend/recom/lib/usecases/shopkeeper/create_product.ex
@@ -6,63 +6,6 @@ defmodule Recom.Usecases.Shopkeeper do
                 {:ok, Entities.Product.t()} | {:error, :duplicate_product} | :error
   end
 
-  defmodule ProductValidatorBehaviour do
-    @type error :: {atom, nonempty_list(atom)}
-    @type errors :: list(error())
-
-    @callback validate(product :: Product.t()) :: {:validation, errors()}
-  end
-
-  defmodule ProductValidator do
-    @behaviour ProductValidatorBehaviour
-
-    @impl true
-    def validate(product) do
-      validation_errors =
-        initial_validation_state()
-        |> validate_quantity(product)
-        |> validate_price(product)
-        |> validate_name(product)
-        |> validate_time_span(product)
-
-      {:validation, validation_errors}
-    end
-
-    defp initial_validation_state(), do: []
-
-    defp validate_price(validation_state, product) do
-      if product.price < 0 do
-        [{:price, [:negative]} | validation_state]
-      else
-        validation_state
-      end
-    end
-
-    defp validate_quantity(validation_state, product) do
-      if product.quantity < 0 do
-        [{:quantity, [:negative]} | validation_state]
-      else
-        validation_state
-      end
-    end
-
-    defp validate_name(validation_state, product) do
-      if String.trim(product.name) == "" do
-        [{:name, [:empty]} | validation_state]
-      else
-        validation_state
-      end
-    end
-
-    defp validate_time_span(validation_state, product) do
-      if product.time_span == nil do
-        [{:time_span, [:blank]} | validation_state]
-      else
-        validation_state
-      end
-    end
-  end
-
   defmodule Notifier do
     @callback notify_of_product_creation(Product.t()) :: :ok
   end
@@ -73,13 +16,11 @@ defmodule Recom.Usecases.Shopkeeper do
       @callback create(Product.t()) :: result
     end
 
-    def create(product, with_validator: validator, with_gateway: gateway, with_notifier: notifier) do
-      with {:validation, []} <- validator.validate(product),
-           {:ok, product} <- gateway.store(product),
+    def create(product, with_gateway: gateway, with_notifier: notifier) do
+      with {:ok, product} <- gateway.store(product),
            :ok <- notifier.notify_of_product_creation(product) do
         {:ok, product}
       else
-        {:validation, errors} -> {:error, {:validation, errors}}
         {:error, :duplicate_product} -> {:error, :duplicate_product}
         :error -> :error
       end
--- backend/recom/test/usecases/shopkeeper/create_product/create_product_test.exs
+++ backend/recom/test/usecases/shopkeeper/create_product/create_product_test.exs
@@ -8,19 +8,13 @@ defmodule Recom.Usecases.Shopkeeper.CreateProductTest do
 
   setup :verify_on_exit!
 
-  defmock(Shopkeeper.ProductValidatorDouble, for: Shopkeeper.ProductValidatorBehaviour)
   defmock(Shopkeeper.ProductsGatewayDouble, for: Shopkeeper.ProductsGateway)
   defmock(Shopkeeper.NotifierDouble, for: Shopkeeper.Notifier)
 
   describe "original product" do
     setup do
-      stub(Shopkeeper.ProductValidatorDouble, :validate, fn :__original_product__ ->
-        {:validation, []}
-      end)
-
       %{
         deps: [
-          with_validator: Shopkeeper.ProductValidatorDouble,
           with_gateway: Shopkeeper.ProductsGatewayDouble,
           with_notifier: Shopkeeper.NotifierDouble
         ]
@@ -53,51 +47,24 @@ defmodule Recom.Usecases.Shopkeeper.CreateProductTest do
 
   describe "duplicate product" do
     test "it returns an error" do
-      stub(Shopkeeper.ProductValidatorDouble, :validate, fn :__duplicate_product__ ->
-        {:validation, []}
-      end)
-
       expect(Shopkeeper.ProductsGatewayDouble, :store, fn :__duplicate_product__ ->
         {:error, :duplicate_product}
       end)
 
       assert {:error, :duplicate_product} ==
                Shopkeeper.CreateProduct.create(:__duplicate_product__,
-                 with_validator: Shopkeeper.ProductValidatorDouble,
                  with_gateway: Shopkeeper.ProductsGatewayDouble,
                  with_notifier: nil
                )
     end
   end
 
-  describe "semantically invalid product" do
-    test "it returns an error" do
-      stub(Shopkeeper.ProductValidatorDouble, :validate, fn :__invalid_product__ ->
-        {:validation, :__a_validation_error__}
-      end)
-
-      result =
-        Shopkeeper.CreateProduct.create(:__invalid_product__,
-          with_validator: Shopkeeper.ProductValidatorDouble,
-          with_gateway: nil,
-          with_notifier: nil
-        )
-
-      assert {:error, {:validation, :__a_validation_error__}} == result
-    end
-  end
-
   describe "handling of a gateway failure" do
     test "it returns an error" do
-      stub(Shopkeeper.ProductValidatorDouble, :validate, fn :__valid_product__ ->
-        {:validation, []}
-      end)
-
       stub(Shopkeeper.ProductsGatewayDouble, :store, fn :__valid_product__ -> :error end)
 
       assert :error ==
                Shopkeeper.CreateProduct.create(:__valid_product__,
-                 with_validator: Shopkeeper.ProductValidatorDouble,
                  with_gateway: Shopkeeper.ProductsGatewayDouble,
                  with_notifier: nil
                )
