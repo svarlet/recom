07cf3f5301daccd5a299f795858ced16f58ec511
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-07-10 16:42:55 +0100

    Combine 2 tests into a single one

--- backend/recom/test/api/controllers/shopkeeper/create_product_controller_test.exs
+++ backend/recom/test/api/controllers/shopkeeper/create_product_controller_test.exs
@@ -35,12 +35,8 @@ defmodule Recom.Api.Shopkeeper.CreateProductControllerTest do
     end
 
     @tag name: 1
-    test "it sends a response", context do
-      assert context.response.state == :sent
-    end
-
-    @tag name: 1
     test "when the name is not a string, it responds with a 422 status code", context do
+      assert context.response.state == :sent
       assert context.response.status == 422
     end
 
