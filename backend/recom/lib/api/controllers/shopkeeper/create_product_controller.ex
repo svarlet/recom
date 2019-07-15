e00aea84decbb9f4b118c2b96e53bac510dc14b6
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-07-15 15:19:39 +0100

    When the payload represent a product, the controller delegates the creation of the product to the usecase

--- backend/recom/lib/api/controllers/shopkeeper/create_product_controller.ex
+++ backend/recom/lib/api/controllers/shopkeeper/create_product_controller.ex
@@ -1,10 +1,31 @@
+defmodule Recom.Api.Shopkeeper.CreateProductPayloadScanner do
+  @callback scan(map()) :: ScanningError.t()
+
+  defmodule ScanningError do
+    defstruct [:message, :errors]
+  end
+end
+
+defmodule Recom.Api.Shopkeeper.CreateProductPresenter do
+  @callback present(ScanningError.t()) :: String.t()
+end
+
 defmodule Recom.Api.Shopkeeper.CreateProductController do
   import Plug.Conn, only: [send_resp: 3, put_resp_header: 3]
 
-  def create_product(conn, with_scanner: scanner, with_usecase: _, with_presenter: presenter) do
+  alias Recom.Api.Shopkeeper.CreateProductPayloadScanner.ScanningError
+  alias Recom.Entities.Product
+
+  def create_product(conn, with_scanner: scanner, with_usecase: usecase, with_presenter: presenter) do
     body =
-      conn.params
-      |> scanner.scan()
+      case scanner.scan(conn.params) do
+        %ScanningError{} = error ->
+          error
+
+        %Product{} = product ->
+          {:ok, saved_product} = usecase.create(product)
+          saved_product
+      end
       |> presenter.present()
 
     conn
--- backend/recom/test/api/controllers/shopkeeper/create_product_controller_test.exs
+++ backend/recom/test/api/controllers/shopkeeper/create_product_controller_test.exs
@@ -1,17 +1,6 @@
-defmodule Recom.Api.Shopkeeper.CreateProductPayloadScanner do
-  @callback scan(map()) :: ScanningError.t()
-
-  defmodule ScanningError do
-    defstruct [:message, :errors]
-  end
-end
-
-defmodule Recom.Api.Shopkeeper.CreateProductPresenter do
-  @callback present(ScanningError.t()) :: String.t()
-end
-
 defmodule Recom.Api.Shopkeeper.CreateProductControllerTest do
   use ExUnit.Case, async: true
+  use Timex
 
   import Mox
   import Plug.Test, only: [conn: 3]
@@ -23,9 +12,12 @@ defmodule Recom.Api.Shopkeeper.CreateProductControllerTest do
   alias Recom.Api.Shopkeeper.CreateProductPresenter
   alias Recom.Api.Shopkeeper.CreateProductPayloadScanner
   alias Recom.Api.Shopkeeper.CreateProductPayloadScanner.ScanningError
+  alias Recom.Usecases.Shopkeeper.CreateProduct
+  alias Recom.Entities.Product
 
   defmock(CreateProductPayloadScanner.Stub, for: CreateProductPayloadScanner)
   defmock(CreateProductPresenter.Stub, for: CreateProductPresenter)
+  defmock(CreateProduct.Mock, for: CreateProduct.Behaviour)
 
   describe "invalid json payload" do
     setup do
@@ -57,6 +49,37 @@ defmodule Recom.Api.Shopkeeper.CreateProductControllerTest do
 
     test "it delegates the creation of the response body to the presenter", context do
       assert context.response.resp_body == "the body"
+    end
+  end
+
+  describe "payload for an original product" do
+    test "it delegates the creation of the product to the usecase" do
+      new_product_payload = %{
+        "name" => "irrelevant",
+        "price" => 1,
+        "quantity" => 1,
+        "from" => "2019-01-31T13:00:00.000000Z",
+        "end" => "2019-02-01T13:00:00.000000Z"
+      }
+
+      product = %Product{
+        name: "irrelevant",
+        price: 1,
+        quantity: 1,
+        time_span: Interval.new(from: ~U[2019-01-31 13:00:00.000000Z], until: [days: 1])
+      }
+
+      stub(CreateProductPayloadScanner.Stub, :scan, fn _ -> product end)
+      expect(CreateProduct.Mock, :create, fn ^product -> {:ok, product} end)
+      stub(CreateProductPresenter.Stub, :present, fn _ -> "irrelevant body" end)
+
+      :post
+      |> conn("/create_product", new_product_payload)
+      |> CreateProductController.create_product(
+        with_scanner: CreateProductPayloadScanner.Stub,
+        with_usecase: CreateProduct.Mock,
+        with_presenter: CreateProductPresenter.Stub
+      )
     end
   end
 end
