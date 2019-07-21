f53606db1c4718c87433ef176f2e79d7db1edfeb
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-07-22 00:40:44 +0100

    When name is missing from the payload, the scanner returns an error

--- backend/recom/test/api/controllers/shopkeeper/create_product/product_scanner_test.exs
+++ backend/recom/test/api/controllers/shopkeeper/create_product/product_scanner_test.exs
@@ -1,5 +1,6 @@
 defmodule Recom.Api.Shopkeeper.CreateProduct.ProductScanner do
   use Timex
+  use Exceptional
 
   alias Recom.Entities.Product
 
@@ -7,29 +8,42 @@ defmodule Recom.Api.Shopkeeper.CreateProduct.ProductScanner do
     defexception ~w{message reason}a
   end
 
+  def scan(nil) do
+    %ScanningError{message: "Nil payload."}
+  end
+
   def scan(payload) do
-    case payload do
-      nil ->
-        %ScanningError{message: "Nil payload."}
+    payload
+    |> check_name()
+    ~> to_product()
+  end
 
-      %{"name" => name} when not is_binary(name) ->
-        %ScanningError{
-          message: "Invalid payload.",
-          reason: %{name: "Invalid type, expected a string."}
-        }
+  defp check_name(%{"name" => name} = payload) when is_binary(name) do
+    payload
+  end
 
-      payload ->
-        %Product{
-          name: payload["name"],
-          price: payload["price"],
-          quantity: payload["quantity"],
-          time_span:
-            Interval.new(
-              from: Timex.parse!(payload["from"], "{ISO:Extended:Z}"),
-              until: [days: 8]
-            )
-        }
-    end
+  defp check_name(%{"name" => _}) do
+    %ScanningError{
+      message: "Invalid payload.",
+      reason: %{name: "Invalid type, expected a string."}
+    }
+  end
+
+  defp check_name(_) do
+    %ScanningError{message: "Invalid payload.", reason: %{name: "Missing."}}
+  end
+
+  defp to_product(payload) do
+    %Product{
+      name: payload["name"],
+      price: payload["price"],
+      quantity: payload["quantity"],
+      time_span:
+        Interval.new(
+          from: Timex.parse!(payload["from"], "{ISO:Extended:Z}"),
+          until: [days: 8]
+        )
+    }
   end
 end
 
@@ -80,6 +94,20 @@ defmodule Recom.Api.Shopkeeper.CreateProduct.ProductScannerTest do
     assert %ScanningError{
              message: "Invalid payload.",
              reason: %{name: "Invalid type, expected a string."}
+           } = ProductScanner.scan(payload)
+  end
+
+  test "name is missing" do
+    payload = %{
+      "price" => 589,
+      "quantity" => 1_000,
+      "from" => "2019-01-01T14:00:00.000000Z",
+      "end" => "2019-01-09T14:00:00.000000Z"
+    }
+
+    assert %ScanningError{
+             message: "Invalid payload.",
+             reason: %{name: "Missing."}
            } = ProductScanner.scan(payload)
   end
 end
