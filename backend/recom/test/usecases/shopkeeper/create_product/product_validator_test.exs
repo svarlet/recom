dd13f146cccd3e61b2ac3c6ddd038b9eabfa4244
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-07-22 10:24:33 +0100

    Extract a setup function to reduce duplication in tests

--- backend/recom/test/usecases/shopkeeper/create_product/product_validator_test.exs
+++ backend/recom/test/usecases/shopkeeper/create_product/product_validator_test.exs
@@ -10,6 +10,9 @@ defmodule Recom.Usecases.Shopkeeper.CreateProduct.ProductValidator do
 
       product.price < 0 ->
         %ValidationError{message: "Invalid product.", reason: %{price: "The price is negative."}}
+
+      true ->
+        product
     end
   end
 end
@@ -22,38 +25,46 @@ defmodule Recom.Usecases.Shopkeeper.CreateProduct.ProductValidatorTest do
   alias Recom.Usecases.Shopkeeper.CreateProduct.ProductValidator
   alias Recom.Usecases.Shopkeeper.CreateProduct.ProductValidator.ValidationError
 
-  test "name is blank" do
-    product = %Product{
-      name: "    \t \n  \r\n ",
-      price: 1,
-      quantity: 1,
-      time_span:
-        Interval.new(
-          from: ~U[2018-10-15 14:00:00.000000Z],
-          until: [days: 1]
-        )
-    }
+  @a_valid_product %Product{
+    name: "Oranges 2kg",
+    price: 1,
+    quantity: 1,
+    time_span:
+      Interval.new(
+        from: ~U[2018-10-15 14:00:00.000000Z],
+        until: [days: 1]
+      )
+  }
 
-    assert %ValidationError{message: "Invalid product.", reason: %{name: "The value is blank."}} ==
-             ProductValidator.validate(product)
+  setup context do
+    a_valid_product = @a_valid_product
+
+    overriden_product =
+      if context[:overrides] do
+        Enum.reduce(context[:overrides], a_valid_product, fn
+          {:blank, field}, product -> Map.put(product, field, "   \r   \t \n   ")
+          {:negative, field}, product -> Map.put(product, field, -1)
+          _, _ -> raise "Unsupported override instruction."
+        end)
+      else
+        a_valid_product
+      end
+
+    [result: ProductValidator.validate(overriden_product)]
   end
 
-  test "price is negative" do
-    product = %Product{
-      name: "Oranges 2kg",
-      price: -1,
-      quantity: 1,
-      time_span:
-        Interval.new(
-          from: ~U[2018-10-15 14:00:00.000000Z],
-          until: [days: 1]
-        )
-    }
+  @tag overrides: [blank: :name]
+  test "name is blank", context do
+    assert %ValidationError{message: "Invalid product.", reason: %{name: "The value is blank."}} ==
+             context.result
+  end
 
+  @tag overrides: [negative: :price]
+  test "price is negative", context do
     assert %ValidationError{
              message: "Invalid product.",
              reason: %{price: "The price is negative."}
-           } == ProductValidator.validate(product)
+           } == context.result
   end
 
   @tag :skip
@@ -62,6 +73,7 @@ defmodule Recom.Usecases.Shopkeeper.CreateProduct.ProductValidatorTest do
   @tag :skip
   test "end precedes from"
 
-  @tag :skip
-  test "valid product"
+  test "valid product", context do
+    assert context.result == @a_valid_product
+  end
 end
