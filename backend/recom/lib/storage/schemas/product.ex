d0290308d7baed7a059980aa61656ade87d4abf5
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-07-22 12:07:52 +0100

    Delete obsolete code

--- backend/recom/lib/storage/schemas/product.ex
+++ backend/recom/lib/storage/schemas/product.ex
@@ -1,6 +1,5 @@
 defmodule Recom.Storage.Product do
   use Ecto.Schema
-  import Ecto.Changeset
 
   schema "products" do
     field(:name, :string)
@@ -10,11 +9,5 @@ defmodule Recom.Storage.Product do
     field(:quantity, :integer)
 
     timestamps(type: :utc_datetime_usec)
-  end
-
-  def changeset(product, params \\ %{}) do
-    product
-    |> cast(params, ~w(name start end price quantity)a)
-    |> unique_constraint(:name, name: :unique_product_name_index)
   end
 end
