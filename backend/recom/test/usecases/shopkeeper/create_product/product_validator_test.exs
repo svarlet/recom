e66131085f72d4a10fa8beac9cc773837116aeaa
Sebastien Varlet <sebastien.varlet@gmail.com>
2019-07-22 09:15:00 +0100

    Create a product validator which returns an error when the name is blank

---
+++ backend/recom/test/usecases/shopkeeper/create_product/product_validator_test.exs
@@ -0,0 +1,48 @@
+defmodule Recom.Usecases.Shopkeeper.CreateProduct.ProductValidator do
+  defmodule ValidationError do
+    defexception ~w{message reason}a
+  end
+
+  def validate(product) do
+    if String.trim(product.name) == "" do
+      %ValidationError{message: "Invalid product.", reason: %{name: "The value is blank."}}
+    end
+  end
+end
+
+defmodule Recom.Usecases.Shopkeeper.CreateProduct.ProductValidatorTest do
+  use ExUnit.Case, async: true
+  use Timex
+
+  alias Recom.Entities.Product
+  alias Recom.Usecases.Shopkeeper.CreateProduct.ProductValidator
+  alias Recom.Usecases.Shopkeeper.CreateProduct.ProductValidator.ValidationError
+
+  test "name is blank" do
+    product = %Product{
+      name: "    \t \n  \r\n ",
+      price: 1,
+      quantity: 1,
+      time_span:
+        Interval.new(
+          from: ~U[2018-10-15 14:00:00.000000Z],
+          until: [days: 1]
+        )
+    }
+
+    assert %ValidationError{message: "Invalid product.", reason: %{name: "The value is blank."}} ==
+             ProductValidator.validate(product)
+  end
+
+  @tag :skip
+  test "price is negative"
+
+  @tag :skip
+  test "quantity is negative"
+
+  @tag :skip
+  test "end precedes from"
+
+  @tag :skip
+  test "valid product"
+end
